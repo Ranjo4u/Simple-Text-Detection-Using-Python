7baeb2f3a9edaaac7667477ea62d4b318f33a23f
Ranjo4u <86999241+Ranjo4u@users.noreply.github.com>
2021-07-08 11:25:31 +0530

    Update letter_bounding_boxes.py

--- letter_bounding_boxes.py
+++ letter_bounding_boxes.py
@@ -3,7 +3,7 @@ from pytesseract import pytesseract
 
 pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-img = cv2.imread("ur_mom.png")
+img = cv2.imread("image.png")
 height, width, c = img.shape
 
 letter_boxes = pytesseract.image_to_boxes(img)
@@ -16,6 +16,6 @@ for box in letter_boxes.splitlines():
 	cv2.rectangle(img,(x,height-y),(w,height-h), (0,0,255),3)
 	cv2.putText(img,box[0],(x,height-h+32),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
-cv2.imwrite('eeeeee.png', img)
+cv2.imwrite('image.png', img)
 cv2.imshow("window", img)
 cv2.waitKey(0)
